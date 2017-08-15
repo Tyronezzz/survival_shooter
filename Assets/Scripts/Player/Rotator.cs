0fb76606ac2b6a0887866de2b0f88a23d8981bd2
Tyronezzz <tyrone@bupt.edu.cn>
2017-08-15 11:58:40 -0400

    first com

---
+++ Assets/Scripts/Player/Rotator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+    private float startWait;
+    private float waveWait;
+
+
+    void Start()
+    {
+        startWait = 1;
+        waveWait = 5;
+        StartCoroutine(weaponWaves());
+        
+    }
+
+
+
+    void Update()           // Update is called once per frame
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+
+    IEnumerator weaponWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            transform.position = new Vector3(Random.Range(-20, 20), 0, Random.Range(-20, 20));
+          //  gameObject.SetActive(true);
+            yield return new WaitForSeconds(waveWait);
+            //
+        }
+
+
+
+    }
+}
