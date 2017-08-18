1d3d066da8d45e8345a2880e39273ee64eb36779
Tyronezzz <tyronehou21@gmail.com>
2017-08-17 22:04:39 -0400

    fire version

---
+++ Assets/Fire.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour {
+
+
+	public float spreadAfter = 1;
+	public float damagepp = 8;
+	public float force = 100;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+	void OnParticleCollision (GameObject other)
+	{
+		Rigidbody body = other.GetComponent<Rigidbody>();
+		other.SendMessageUpwards("ApplyDamage", damagepp, SendMessageOptions.DontRequireReceiver);
+
+		if (body)
+		{
+			Vector3 direction = other.transform.position - transform.position;
+			direction = direction.normalized;
+			body.AddForce (direction * force);
+		}
+
+		//yield return new WaitForSeconds(spreadAfter);
+		if (other != null)
+		{
+			other.SendMessageUpwards("Fire", null, SendMessageOptions.DontRequireReceiver);
+		}
+	}
+}
---
+++ Assets/Scripts/Player/Rotator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+    private float startWait;
+    private float waveWait;
+	public GameObject pickupp;
+
+    void Start()
+    {
+        startWait = 5;
+        waveWait = 8;
+        StartCoroutine(weaponWaves());
+        
+    }
+
+
+
+    void Update()           // Update is called once per frame
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+
+    IEnumerator weaponWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+
+        while (true)
+        {
+			GameObject pckup2 = pickupp;
+
+			Vector3 player_pos = GameObject.Find ("Player").transform.position;
+
+			transform.position = new Vector3(Random.Range(player_pos[0]-5, player_pos[0]+5), 0, Random.Range(player_pos[2]-5, player_pos[2]+5));
+			Instantiate (pckup2, transform.position, Quaternion.identity);
+
+			yield return new WaitForSeconds(waveWait);
+        }
+
+    }
+}
